761e3a2ed7ec75b26c518ddbe2258496697832c2
Edvard Boguslavskij <edvard.bgs@gmail.com>
2025-09-08 22:12:49 +0200

    add multi machine support

---
+++ work/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Work machine nix-darwin configuration";
+
+  inputs = {
+    personal-config.url = "path:..";
+    lunar-tools = {
+      url = "git+ssh://git@github.com/lunarway/lw-nix?ref=feat/zsh-plugin";
+    };
+  };
+
+  outputs = { personal-config, lunar-tools, ... }:
+  let
+    inherit (personal-config.lib) mkSystem;
+  in
+  {
+    darwinConfigurations.work-mac = mkSystem {
+      user = "edb";
+      homeModule = ../hosts/work/home.nix;
+      hostModule = ../hosts/work;
+      extraOverlays = [ lunar-tools.overlays.default];
+      gitConfig = {
+        userName = "Edvard Boguslavskij";
+        userEmail = "edb@lunar.app";
+        signingKey = "/Users/edb/.ssh/github.pub";
+        workSSHKey = "/Users/edb/.ssh/github";
+        personalSSHKey = "/Users/edb/.ssh/id_ed25519";
+        enableLunarUrls = true;
+        enablePersonalAlias = true;
+        user = "edb";
+      };
+    };
+  };
+}
