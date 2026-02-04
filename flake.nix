78e54b84c7b4d6fab8cb96858e832f5a1560e4f1
Edvard Boguslavskij <edvard.bgs@gmail.com>
2026-02-04 09:13:52 +0100

    update to more stable version of nix-pkgs

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Base nix-darwin configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
-      url = "github:nix-darwin/nix-darwin";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
